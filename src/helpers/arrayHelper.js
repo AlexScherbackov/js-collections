b0e734fb2eeb56f5a281ddba669aa01184c290e5
AlexScherbackov <alsherbakov13@gmail.com>
2020-01-16 10:04:08 +0400

    js-collections-0001 added arrayHelpers and uniq function

---
+++ src/helpers/arrayHelper.js
@@ -0,0 +1,2 @@
+export const uniq = (arr) => arr.filter((item, index) => arr.indexOf(item) === index);
+
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { uniq } from './helpers/arrayHelper.js';
+const arra = [1,1,2,1,3,4];
+console.log(uniq(arra));
