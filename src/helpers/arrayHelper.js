318dca451e6fea2294b5627d63af6ceb27f5757f
AlexScherbackov <alsherbakov13@gmail.com>
2020-01-21 13:55:16 +0400

    js-collections-0010 add objectify functiion to arrayHelper.js

--- src/helpers/arrayHelper.js
+++ src/helpers/arrayHelper.js
@@ -1,2 +1,17 @@
-export const uniq = (arr) => arr.filter((item, index) => arr.indexOf(item) === index);
+const uniq = (arr) => arr.filter((item, index) => arr.indexOf(item) === index);
 
+const objectify = (list, selector) => {
+  const reducer = (acc, item) => Object.defineProperty(
+    acc,
+    selector(item),
+    {
+      enumerable: true,
+      configurable: false,
+      writable: false,
+      value: item,
+    },
+  );
+  return list.reduce(reducer, {});
+};
+
+export { uniq, objectify };
