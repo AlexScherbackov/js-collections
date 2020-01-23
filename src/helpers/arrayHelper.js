337275635f2c561c4663cf6c02724505679e7995
AlexScherbackov <alsherbakov13@gmail.com>
2020-01-23 14:01:57 +0400

    js-collections-0012 add arrayOperations

--- src/helpers/arrayHelper.js
+++ src/helpers/arrayHelper.js
@@ -1,5 +1,25 @@
+import { arrayOps } from '../constants/arrayOperations.js';
+
 const uniq = (arr) => arr.filter((item, index) => arr.indexOf(item) === index);
 
 const objectify = (coll, select) => coll.reduce((acc, item) => ({ ...acc, [select(item)]: item }), {});
 
-export { uniq, objectify };
+const similarPropsCount = (list, prop) => {
+  const reducer = (acc, { prop }) => {
+    if (acc.hasOwnProperty(prop)) {
+      acc[prop] += 1;
+    } else {
+      acc[prop] = 1;
+    }
+
+    return acc;
+  };
+  return list.reduce(reducer, {});
+};
+
+const applyOpToCollection = (coll, operationName, index, value, ops = arrayOps) => (
+  ops.has(operationName) ? ops.get(operationName)(coll, index, value) : coll
+);
+
+
+export { uniq, objectify, similarPropsCount, applyOpToArray };
